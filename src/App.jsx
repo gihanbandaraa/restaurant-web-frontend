70167e57d998177377bfb04949f332c011c0b7c8
Gihan Bandara <63912749+gihanbandaraa@users.noreply.github.com>
2024-07-16 12:10:14 +0530

    Add Browser routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Home from "./pages/Home";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import AboutUs from "./pages/AboutUs";
+import NavBar from "./components/NavBar";
+
 function App() {
   return (
-    <div className="App">
-      Hi
-    </div>
-  )
+    <BrowserRouter>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/about-us" element={<AboutUs />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
