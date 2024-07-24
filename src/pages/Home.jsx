3a2609bcef7e87d3af9383dda4713a0f3a128a37
Gihan Bandara <63912749+gihanbandaraa@users.noreply.github.com>
2024-07-24 18:03:04 +0530

    create the hero section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React from "react";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <section
+      id="hero"
+      className="relative mt-5 min-h-screen bg-max bg-hero-pattern bg-center bg-cover md:bg-none"
+    >
+      <div className="absolute inset-0 bg-white opacity-75 md:hidden"></div>
+      <div className="relative flex justify-evenly flex-col md:flex-row items-center h-screen mx-10">
+        <div className="max-w-lg lg:mb-20 ">
+          <h2 className="text-4xl lg:text-6xl font-bold">
+            Meet, Eat & Enjoy The{" "}
+            <span className="text-red-500">True Taste</span>.
+          </h2>
+          <p className="mt-2 font-semibold text-gray-500 text-sm">
+            Food tastes better when you eat it with your family and friends
+          </p>
+          <div>
+            <button className="mt-5 bg-red-500 font-medium text-white px-5 py-2 rounded-md">
+              Get Started
+            </button>
+          </div>
+        </div>
+        <div>
+          <img
+            src="/Images/Image.png"
+            alt="rice-bowl"
+            className="max-h-[650px] hidden md:block"
+          />
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default Home
+export default Home;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'hero-pattern': "url('/Images/Image.png')",
+      }
+    },
   },
   plugins: [],
 };
