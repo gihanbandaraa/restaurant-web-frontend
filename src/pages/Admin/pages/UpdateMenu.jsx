b0e495b18af1d0b70d95a4c404d1182c27740b89
Gihan Bandara <63912749+gihanbandaraa@users.noreply.github.com>
2024-08-10 23:31:28 +0530

    crud operations completed in the add menu

--- src/pages/Admin/pages/UpdateMenu.jsx
+++ src/pages/Admin/pages/UpdateMenu.jsx
@@ -1,16 +1,23 @@
 import React, { useEffect, useState } from "react";
-import { FaPlusCircle } from "react-icons/fa";
+import { FaPlusCircle, FaEdit } from "react-icons/fa";
+import { IoClose } from "react-icons/io5";
 import useAlert from "../../../hooks/useAlert";
 import Alert from "../../../components/Alert";
 import MenuItemCard from "../components/MenuItemCard";
 import AddItemModal from "../components/AddItemModel";
+import EditItemModal from "../components/EditItemModel";
+import { getStorage, ref, deleteObject } from "firebase/storage";
 
 const UpdateMenu = () => {
   const [loading, setLoading] = useState(true);
   const [categories, setCategories] = useState([]);
   const [selectedCategory, setSelectedCategory] = useState(null);
   const [menuItems, setMenuItems] = useState([]);
-  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isAddModalOpen, setIsAddModalOpen] = useState(false);
+  const [isEditModalOpen, setIsEditModalOpen] = useState(false);
+  const [itemToEdit, setItemToEdit] = useState(null);
+  const [confirmDeleteModalOpen, setConfirmDeleteModalOpen] = useState(false);
+  const [itemToDelete, setItemToDelete] = useState(null);
   const { alertInfo, handleShowAlert, handleCloseAlert } = useAlert();
 
   const fetchCategories = async () => {
@@ -60,18 +67,60 @@ const UpdateMenu = () => {
     fetchMenuItems();
   }, [selectedCategory]);
 
-  // Callback function to refetch menu items when an item is added
-  const handleItemAdded = () => {
+  const handleItemUpdated = () => {
     fetchMenuItems();
+  };
+
+  const handleEditClick = (item) => {
+    setItemToEdit(item);
+    setIsEditModalOpen(true);
+  };
+
+  const handleDeleteClick = (item) => {
+    setItemToDelete(item);
+    setConfirmDeleteModalOpen(true);
+  };
+
+  const closeConfirmDeleteModal = () => {
+    setConfirmDeleteModalOpen(false);
+    setItemToDelete(null);
+  };
+
+  const handleConfirmDelete = async () => {
+    if (itemToDelete) {
+      try {
+        const res = await fetch(`/api/admin/delete-menu/${itemToDelete._id}`, {
+          method: "DELETE",
+        });
+        const data = await res.json();
+        if (res.ok) {
+          handleShowAlert("success", "Item deleted successfully");
+          fetchMenuItems();
+        } else {
+          handleShowAlert("error", data.message);
+        }
+      } catch (error) {
+        handleShowAlert("error", "Something went wrong!");
+      } finally {
+        closeConfirmDeleteModal();
+      }
+    }
   };
 
   return (
     <section className="fixed-container max-h-screen overflow-y-auto">
+      <Alert
+        type={alertInfo.type}
+        message={alertInfo.message}
+        isOpen={alertInfo.isOpen}
+        onClose={handleCloseAlert}
+      />
+
       <div className="py-5 max-w-screen-lg mx-auto">
         <div className="flex flex-row justify-between items-center mb-4">
           <h1 className="text-sm md:text-xl font-bold">Menu Items</h1>
           <div
-            onClick={() => setIsModalOpen(true)}
+            onClick={() => setIsAddModalOpen(true)}
             className="flex items-center space-x-2 px-2 md:px-4 md:py-2 py-1 rounded-lg text-white font-poppins font-medium bg-red-500 hover:bg-red-400 transform duration-300 cursor-pointer"
           >
             <FaPlusCircle className="text-xs md:text-sm" />
@@ -79,7 +128,6 @@ const UpdateMenu = () => {
           </div>
         </div>
 
-        {/* Horizontal list of categories */}
         <div className="flex hide-scrollbar overflow-x-auto space-x-4 mb-6">
           <button
             className={`px-2 py-1 md:px-4 md:py-2 text-sm md:text-base rounded-lg ${
@@ -95,7 +143,7 @@ const UpdateMenu = () => {
           {categories.map((category) => (
             <button
               key={category._id}
-              className={`px-2 py-1 md:px-4 md:py-2 text-sm md:text-base rounded-lg ${
+              className={`px-2 py-1 md:px-4 md:py-2 text-sm rounded-lg ${
                 selectedCategory === category._id
                   ? "bg-red-500 text-white"
                   : "bg-gray-200 text-gray-700"
@@ -113,7 +161,6 @@ const UpdateMenu = () => {
           </div>
         )}
 
-        {/* Display message if no items available */}
         {!loading && menuItems.length === 0 && (
           <p className="text-center mt-10 text-gray-500">No items available.</p>
         )}
@@ -121,27 +168,72 @@ const UpdateMenu = () => {
         <div className="overflow-y-auto max-h-[calc(100vh-200px)]">
           {!loading &&
             menuItems.map((item) => (
-              <MenuItemCard key={item._id} item={item} />
+              <div key={item._id}>
+                <MenuItemCard
+                  item={item}
+                  handleEdit={handleEditClick}
+                  handleDelete={handleDeleteClick}
+                />
+              </div>
             ))}
         </div>
 
-        {/* Alert component */}
-        <Alert
-          type={alertInfo.type}
-          message={alertInfo.message}
-          isOpen={alertInfo.isOpen}
-          onClose={handleCloseAlert}
-        />
+        {/* Confirmation Delete Modal */}
+        <div
+          className={`fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50 transition-opacity duration-300 ${
+            confirmDeleteModalOpen
+              ? "opacity-100 visible"
+              : "opacity-0 invisible"
+          }`}
+        >
+          <div
+            className={`bg-white rounded-lg p-6 w-full max-w-sm transform transition-transform duration-300 ${
+              confirmDeleteModalOpen ? "scale-100" : "scale-95"
+            }`}
+          >
+            <div className="flex justify-between items-center mb-4">
+              <h2 className="text-xl font-bold">Confirm Deletion</h2>
+              <IoClose
+                className="text-2xl cursor-pointer"
+                onClick={closeConfirmDeleteModal}
+              />
+            </div>
+            <p className="mb-4">Are you sure you want to delete this item?</p>
+            <div className="flex justify-end space-x-4">
+              <button
+                onClick={handleConfirmDelete}
+                className="bg-red-500 text-white px-4 py-2 rounded-md hover:bg-red-400 transform duration-300"
+              >
+                Confirm
+              </button>
+              <button
+                onClick={closeConfirmDeleteModal}
+                className="bg-gray-500 text-white px-4 py-2 rounded-md hover:bg-gray-400 transform duration-300"
+              >
+                Cancel
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
 
-      {/* Add Item Modal */}
       <AddItemModal
         selectedCategory={selectedCategory}
         categories={categories}
-        isOpen={isModalOpen}
+        isOpen={isAddModalOpen}
         onClose={() => {
-          setIsModalOpen(false);
-          handleItemAdded(); // Refetch menu items when modal closes
+          setIsAddModalOpen(false);
+          handleItemUpdated();
+        }}
+      />
+
+      <EditItemModal
+        itemData={itemToEdit}
+        categories={categories}
+        isOpen={isEditModalOpen}
+        onClose={() => {
+          setIsEditModalOpen(false);
+          handleItemUpdated();
         }}
       />
     </section>
